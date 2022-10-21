3256f17ab4c3f0179b2ae9c55afbe579812a01e7
Ethan De La Rosa <dlrethan10@gmail.com>
2022-10-21 03:17:44 -0700

    Added homepage added starter homepage not finished yet

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,16 +2,11 @@ var requestOptions = {
   method: "GET",
   redirect: "follow",
 };
-function printTitles() {
-  // function for printing our game deal data
-  fetch(
-    "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15",
-    requestOptions
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => console.log(data))
-    .catch((error) => console.log("error", error));
-}
-printTitles();
+
+fetch(
+  "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15",
+  requestOptions
+)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.log("error", error));
