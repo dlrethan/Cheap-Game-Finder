74a87aeb985e43e6b26123dffa11ba9164d00d0c
Ethan De La Rosa <dlrethan10@gmail.com>
2022-10-20 21:18:03 -0700

    Changed results to json

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,11 +2,16 @@ var requestOptions = {
   method: "GET",
   redirect: "follow",
 };
-
-fetch(
-  "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15",
-  requestOptions
-)
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.log("error", error));
+function printTitles() {
+  // function for printing our game deal data
+  fetch(
+    "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15",
+    requestOptions
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => console.log(data))
+    .catch((error) => console.log("error", error));
+}
+printTitles();
